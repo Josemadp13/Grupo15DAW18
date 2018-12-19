f65f1cdd982a1efd6ff56df8fb714598a72951ea
natibd <45976116+natibd@users.noreply.github.com>
2018-12-19 12:27:19 +0100

    Add files via upload
    oliiii, os subo mis archivos

---
+++ app/Config.php
@@ -0,0 +1,11 @@
+<?php
+
+ class Config
+ {
+     static public $mvc_bd_hostname = "localhost";
+     static public $mvc_bd_nombre   = "alimentos";
+     static public $mvc_bd_usuario  = "mynatibd19";
+     static public $mvc_bd_clave    = "n5sZ7jVs";
+     static public $mvc_vis_css     = "estilo.css";
+ }
+?>
